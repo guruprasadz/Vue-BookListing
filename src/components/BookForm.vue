7399f0bc252de582128ee666bac5b877f86b3973
Guruprasad Zapate <guruprasad.zapate@siemens.com>
2019-08-19 16:20:53 +0200

    add bookform component

---
+++ src/components/BookForm.vue
@@ -0,0 +1,33 @@
+<template>
+    <form v-on:submit.prevent="bookSubmit(bookTitle,bookAuthor)"  class="" action="#" method="post">
+        <input v-model="bookTitle" type="text" value="" placeholder="Book Title">
+        <input v-model="bookAuthor" type="text" value="" placeholder="Book Author">
+        <button type="submit" name="button">Add Book</button>
+    </form>
+</template>
+<script>
+
+import BookItem from './BookItem';
+export default {
+    name: 'BookForm',
+    props: ['books'],
+    components:{
+        BookItem
+    },
+    data() {
+        return {
+            bookTitle:  '',
+            bookAuthor: ''
+        }
+        
+    },
+    methods:{
+        bookSubmit(bookTitle,bookAuthor) {
+            this.$emit('addBook', bookTitle,bookAuthor);
+        }
+    }
+    
+}
+</script>
+<style>
+</style>
